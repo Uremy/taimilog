630b142a6f767513f7b02f9fdb1a78df87c0b762
uremy <uremy111@outlook.com>
2026-01-25 19:57:50 -0600

    feat: update homepage content and add Speed Insights integration

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,15 +2,20 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </div>
+    <main className="flex flex-1 flex-col justify-center text-center">
+      <div className="container relative z-10 flex flex-col items-center gap-8 py-24 lg:py-40">
+        
+        {/* TÍTULO PRINCIPAL */}
+        <h1 className="text-5xl font-bold md:text-7xl">
+          Hola, soy <span className="text-fd-primary">Uremy</span>
+        </h1>
+
+        {/* DESCRIPCIÓN */}
+        <p className="max-w-2xl text-lg text-fd-muted-foreground md:text-xl">
+          Estudiante de medicina. 
+          Aquí comparto mis apuntes de estudio y reflexiones sobre lo que voy aprendiendo.
+        </p>
+      </div>
+    </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from 'next';
 import 'katex/dist/katex.css';
 
@@ -56,6 +57,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         >
           {children}
         </RootProvider>
+        <SpeedInsights />
       </body>
     </html>
   );

bfaad53d174e7c802170672f14423c8a908d586c
Uremy <uremy111@outlook.com>
2026-08-17 23:05:27 -0600

    Eliminacion del nomograma

--- components/science-charts/index.ts
+++ components/science-charts/index.ts
@@ -2,10 +2,8 @@
 
 export * from './diagrams/SandboxChart';
 export * from './diagrams/FlowVolumeLoop';
-export * from './diagrams/WiggersDiagram';
-export * from './diagrams/AcidBaseMapChart';
+export * from './diagrams/WiggersDiagram'
 
 // Exportación opcional de datasets por si quieres crear variantes en notas MDX
 export * from './diagrams/data/spirometry';
 export * from './diagrams/data/wiggers';
-export * from './diagrams/data/acidBaseMap'
